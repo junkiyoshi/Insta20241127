f9f647fd98c618b468e5adb74666f9b2a0eb4919
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2024-11-27 20:04:33 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,224 @@
+#include "ofApp.h"	
+
+//--------------------------------------------------------------
+Actor::Actor(vector<glm::vec3>& location_list, vector<vector<int>>& next_index_list, vector<int>& destination_list) {
+
+	this->select_index = ofRandom(location_list.size());
+	while (true) {
+
+		auto itr = find(destination_list.begin(), destination_list.end(), this->select_index);
+		if (itr == destination_list.end()) {
+
+			destination_list.push_back(this->select_index);
+			break;
+		}
+
+		this->select_index = (this->select_index + 1) % location_list.size();
+	}
+
+	this->next_index = this->select_index;
+}
+
+//--------------------------------------------------------------
+void Actor::update(const int& frame_span, vector<glm::vec3>& location_list, vector<vector<int>>& next_index_list, vector<int>& destination_list) {
+
+	if (ofGetFrameNum() % frame_span == 0) {
+
+		auto tmp_index = this->select_index;
+		this->select_index = this->next_index;
+		int retry = next_index_list[this->select_index].size();
+		this->next_index = next_index_list[this->select_index][(int)ofRandom(next_index_list[this->select_index].size())];
+		while (--retry > 0) {
+
+			auto destination_itr = find(destination_list.begin(), destination_list.end(), this->next_index);
+			if (destination_itr == destination_list.end()) {
+
+				if (tmp_index != this->next_index) {
+
+					destination_list.push_back(this->next_index);
+					break;
+				}
+			}
+
+			this->next_index = next_index_list[this->select_index][(this->next_index + 1) % next_index_list[this->select_index].size()];
+		}
+		if (retry <= 0) {
+
+			destination_list.push_back(this->select_index);
+			this->next_index = this->select_index;
+		}
+	}
+
+	auto param = ofGetFrameNum() % frame_span;
+	auto distance = location_list[this->next_index] - location_list[this->select_index];
+	this->location = location_list[this->select_index] + distance / frame_span * param;
+
+	this->log.push_front(this->location);
+	while (this->log.size() > frame_span * 2 ) { this->log.pop_back(); }
+}
+
+//--------------------------------------------------------------
+void Actor::draw() {
+
+	ofNoFill();
+	
+
+	for (int i = 1; i < this->log.size(); i++) {
+
+		ofSetColor(ofColor(this->color, ofMap(i, 1, this->log.size(), 255, 0)));
+		ofDrawLine(this->log[i - 1], this->log[i]);
+	}
+}
+
+//--------------------------------------------------------------
+glm::vec3 Actor::getLocation() {
+
+	return this->location;
+}
+
+//--------------------------------------------------------------
+deque<glm::vec3> Actor::getLog() {
+
+	return this->log;
+}
+
+//--------------------------------------------------------------
+void Actor::setColor(ofColor color) {
+
+	this->color = color;
+}
+
+//--------------------------------------------------------------
+ofColor Actor::getColor() {
+
+	return this->color;
+}
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(25);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(39);
+	ofSetLineWidth(1.5);
+	ofEnableDepthTest();
+
+	int span = 30;
+	for (int x = -360; x <= 360; x += span) {
+
+		for (int y = -180; y <= 180; y += span) {
+
+			for (int z = -180; z <= 180; z += span) {
+
+				if (abs(x) > 240 || (abs(y) < 60 && abs(z) < 60)) {
+
+					this->location_list.push_back(glm::vec3(x, y, z));
+				}
+			}
+		}
+	}
+
+	for (auto& location : this->location_list) {
+
+		vector<int> next_index = vector<int>();
+		int index = -1;
+		for (auto& other : this->location_list) {
+
+			index++;
+			if (location == other) { continue; }
+
+			float distance = glm::distance(location, other);
+			if (distance <= 30) {
+
+				next_index.push_back(index);
+			}
+		}
+
+		this->next_index_list.push_back(next_index);
+	}
+
+	ofColor color;
+	for (int i = 0; i < 600; i++) {
+
+		this->actor_list.push_back(make_unique<Actor>(this->location_list, this->next_index_list, this->destination_list));
+
+		color.setHsb(ofMap(i, 0, 600, 0, 255), 200, 255);
+		this->actor_list.back()->setColor(color);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	int frame_span = 4;
+	int prev_index_size = 0;
+
+	if (ofGetFrameNum() % frame_span == 0) {
+
+		prev_index_size = this->destination_list.size();
+	}
+
+	for (auto& actor : this->actor_list) {
+
+		actor->update(frame_span, this->location_list, this->next_index_list, this->destination_list);
+	}
+
+	if (prev_index_size != 0) {
+
+		this->destination_list.erase(this->destination_list.begin(), this->destination_list.begin() + prev_index_size);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	this->cam.begin();
+	ofRotateX(ofGetFrameNum() * 0.72);
+
+	for (auto& actor : this->actor_list) {
+
+		actor->draw();
+	}
+
+	this->cam.end();
+
+	/*
+	int start = 125;
+	if (ofGetFrameNum() > start) {
+
+		ostringstream os;
+		os << setw(4) << setfill('0') << ofGetFrameNum() - start;
+		ofImage image;
+		image.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+		image.saveImage("image/cap/img_" + os.str() + ".jpg");
+		if (ofGetFrameNum() - start >= 25 * 20) {
+
+			std::exit(1);
+		}
+	}
+	*/
+}
+
+//--------------------------------------------------------------
+glm::vec3 ofApp::make_point(float R, float r, float u, float v) {
+
+	// 数学デッサン教室 描いて楽しむ数学たち　P.31
+
+	u *= DEG_TO_RAD;
+	v *= DEG_TO_RAD;
+
+	auto x = (R + r * cos(u)) * cos(v);
+	auto y = (R + r * cos(u)) * sin(v);
+	auto z = r * sin(u);
+
+	return glm::vec3(x, y, z);
+}
+
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
